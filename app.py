19ee65edab551af4e225ae01d6211c9e653bee5b
Suraj Negi <surajnegi343@gmail.com>
2020-09-08 02:42:10 +0530

    updated app.py

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def post():
     return render_template('post.html', params=params , songs=songs)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(debug=True, port=port)
